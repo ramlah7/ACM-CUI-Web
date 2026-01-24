4f64690c0408abdd996a8de272436141eff011cf
NextStep-IM <malikharis2003@gmail.com>
2026-01-25 02:25:00 +0500

    Fix name issues

--- backend/api/views/__init__.py
+++ backend/api/views/__init__.py
@@ -3,6 +3,9 @@ from .auth import SignupView, OTPView, LoginView, LogoutView, PasswordChangeView
 from .bill import BillRUDView, BillListCreateView
 from .blog import BlogEditView, BlogDeleteView, BlogUploadView, InlineImageUploadView, BlogListAPIView
 from .event import EventRUDView, EventImageRUDView, EventListCreateView
-from .meeting import MeetingPDFView, MeetingListView, MeetingRUDView, MeetingCreateView, MeetingAttendanceRUDView, MeetingAttendanceListView
+from .meeting import MeetingPDFView, MeetingListView, MeetingRUDView, MeetingCreateView, MeetingAttendanceRUDView, \
+    MeetingAttendanceListView
 from .root import api_root
 from .user import StudentRUView, StudentsListView, PublicStudentsListView
+from .recruitment import RecruitmentSessionViewSet, ApplicationReviewViewSet, ApplicationStatusUpdateViewSet, \
+    ApplicationSubmitView, ActiveRecruitmentSessionView
--- backend/api/views/recruitment.py
+++ backend/api/views/recruitment.py
@@ -1,19 +1,17 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework import status
-from django.utils.timezone import now
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
-
 from api.models import RecruitmentSession, RecruitmentApplication
 from api.serializers.recruitment import (
-    RecruitmentSerializer,
+    RecruitmentSessionSerializer,
     RecruitmentApplicationSubmissionSerializer,
     RecruitmentApplicationSerializer,
 )
-
+from django.utils.timezone import now
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 
 # -------------
 # Admin Views
@@ -24,7 +22,7 @@ class RecruitmentSessionViewSet(ModelViewSet):
     Full CRUD for recruitment sessions, admin only.
     """
     queryset = RecruitmentSession.objects.all()
-    serializer_class = RecruitmentSerializer
+    serializer_class = RecruitmentSessionSerializer
     permission_classes = [IsAuthenticated, IsAdminUser]
 
 
@@ -68,3 +66,26 @@ class ApplicationStatusUpdateViewSet(ModelViewSet):
         application.status = new_status
         application.save()
         return Response({"status": "updated"})
+
+# -----------------------------
+# Public Views
+# -----------------------------
+
+class ActiveRecruitmentSessionView(ReadOnlyModelViewSet):
+    queryset = RecruitmentSession.objects.all()
+    serializer_class = RecruitmentSessionSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        today = now().date()
+        return RecruitmentSession.objects.filter(
+            application_start__lte=today,
+            application_end__gte=today
+        )
+
+class ApplicationSubmitView(ModelViewSet):
+    queryset = RecruitmentApplication.objects.all()
+    serializer_class = RecruitmentApplicationSubmissionSerializer
+    permission_classes = [AllowAny]
+
+    http_method_names = ['post']
