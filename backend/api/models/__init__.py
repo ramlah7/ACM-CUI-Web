36f4f2b0e57dd7fe615fe234986f8a22ff77340c
alesha-Yasin <aleshayasin2004@gmail.com>
2026-01-25 03:27:33 +0500

    Remove extra timestamp fields from RecruitmentApplication model

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -3,4 +3,4 @@ from .bill import Bill
 from .blog import Blog, BlogImage, InlineImage
 from .event import Event, EventImage
 from .meeting import Meeting, MeetingAttendance
-from .recruitment import ( RecruitmentSession, RecruitmentApplications, PersonalInfo, AcademicInfo, RolePreferences )
+from .recruitment import ( RecruitmentSession, RecruitmentApplication, PersonalInfo, AcademicInfo, RolePreferences )
