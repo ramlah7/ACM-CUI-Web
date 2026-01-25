c6f2f7ff777946116f53c9218be9157e8bd59f5a
NextStep-IM <malikharis2003@gmail.com>
2026-01-26 03:21:43 +0500

    Add views for event

--- backend/api/views/event.py
+++ backend/api/views/event.py
@@ -1,60 +1,30 @@
-from rest_framework import generics, status
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from api.models import Event, EventImage
-from api.permissions import IsLeadOrAdmin, is_staff
-from api.serializers import EventSerializer, EventImageEditSerializer
+from rest_framework import generics
+from api.models import Event, EventType
+from api.serializers import (
+    EventSerializer,
+    EventWriteSerializer,
+    EventTypeSerializer
+)
 
 
 class EventListCreateView(generics.ListCreateAPIView):
-    queryset = Event.objects.prefetch_related('images')
-    serializer_class = EventSerializer
-    permission_classes = [AllowAny]
+    queryset = Event.objects.select_related('event_type')
 
-    def create(self, request, *args, **kwargs):
-        if not is_staff(request.user.role):
-            return Response(data=None, status=status.HTTP_403_FORBIDDEN)
-        data = request.data
-        image_list = request.FILES.getlist('images')
-        images = []
-        for image in image_list:
-            images.append({
-                'image': image,
-            })
-        data.setlist('images', images)
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        data = serializer.data
-        return Response(data, status=status.HTTP_201_CREATED)
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return EventWriteSerializer
+        return EventSerializer
 
 
-class EventRUDView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Event.objects.prefetch_related('images')
-    serializer_class = EventSerializer
-    permission_classes = [AllowAny]
+class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Event.objects.select_related('event_type')
 
-    def update(self, request, *args, **kwargs):
-        if not is_staff(request.user.role):
-            return Response(data=None, status=status.HTTP_403_FORBIDDEN)
-        return super().update(request, *args, **kwargs)
-
-    def partial_update(self, request, *args, **kwargs):
-        if not is_staff(request.user.role):
-            return Response(data=None, status=status.HTTP_403_FORBIDDEN)
-        return super().partial_update(request, *args, **kwargs)
-
-    def destroy(self, request, *args, **kwargs):
-        if not is_staff(request.user.role):
-            return Response(data=None, status=status.HTTP_403_FORBIDDEN)
-        return super().destroy(request, *args, **kwargs)
+    def get_serializer_class(self):
+        if self.request.method in ['PUT', 'PATCH']:
+            return EventWriteSerializer
+        return EventSerializer
 
 
-class EventImageRUDView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = EventImage.objects.all()
-    serializer_class = EventImageEditSerializer
-    permission_classes = [IsLeadOrAdmin]
-    lookup_url_kwarg = 'img_pk'
-
-    def get_queryset(self):
-        return EventImage.objects.filter(event_id=self.kwargs['pk'])
+class EventTypeListCreateView(generics.ListCreateAPIView):
+    queryset = EventType.objects.all()
+    serializer_class = EventTypeSerializer
